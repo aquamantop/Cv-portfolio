4c06d76ee0b4cb2e42690cc1a9e9964d6e4ca950
Franco Rampazzo <franco.rodrigo.rampazzo@gmail.com>
2022-09-08 20:27:58 -0300

    basename

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SobreMi from "./components/SobreMi";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/SobreMi' element={<SobreMi></SobreMi>}></Route>
